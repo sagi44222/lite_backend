157b192dcc0cc5f3706e2d5bc6c862b2c72b8039
sagi44222 <sagi4422@gmail.com>
2018-12-02 13:36:03 +0200

    add email pdf

--- controllers/email.controller.js
+++ controllers/email.controller.js
@@ -1,21 +1,17 @@
 const nodemailer = require('nodemailer');
 
 exports.pdfFile = (req, res) => {
-    //if (!req.body) {
-     //   return res.status(400).send({
-      //      message: "Email data cannot be empty"
-       // });
-    //}
+    if (!req.body) {
+        return res.status(400).send({
+            message: "Email data cannot be empty"
+        });
+    }
+    console.log(req.body);
     const outputData = `
-    <p>You have a new contact request</p>
-    <h3>Contact Details</h3>
-    <ul>  
-      <li>Name: ${req.body}</li>
-      <li>Email: ${req.body}</li>
-    </ul>
-    <h3>Message</h3>
-    <p>${req.body}</p>
-  `;
+        <p>Dear ${req.body.name}, </p>
+        <br>
+        <p>Attached Please find your fertilizer plan.</p>
+  ` ;
 
     let transporter = nodemailer.createTransport({
         host: 'smtp.office365.com',
@@ -24,7 +20,7 @@ exports.pdfFile = (req, res) => {
 
         auth: {
             user: 'sagi.gliksman@smart-fertilizer.com',
-            pass: 'mypassword'
+            pass: 'Acdc4422!!'
         },
         tls: {
            // rejectUnauthorized: false
@@ -38,8 +34,14 @@ exports.pdfFile = (req, res) => {
     let HelperOptions = {
         from: 'Info <sagi.gliksman@smart-fertilizer.com>',
         to: 'sagi <sagi4422@gmail.com>',
-        subject: 'Majeni Contact Request',
-        text: 'Hello',
+        subject: 'SMART Fertilizer - Fertilizer plan',
+        text: 'Dear' + req.body.name + "," +'/n' + "Please find attached your fertilizer plan.",
+        attachments: [{
+            filename: 'Smart-TestResults.pdf',
+           // content: req.body.pdf,
+            //encoding: 'base64'
+            path: req.body.pdf
+        }],
         html: outputData
     };
 
@@ -48,10 +50,11 @@ exports.pdfFile = (req, res) => {
             return console.log(error);
 
         }
+
         console.log("The message was sent!");
         console.log(info);
     });
-
+    return res.status(200).send({message: 'Email sent'});
 
 
 
